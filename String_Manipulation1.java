75c7b67746c1ddea44047d0b7e426332136be049
PunuruMeghnaReddy <2420030401@klh.edu.in>
2025-07-31 14:28:08 +0530

    Add files via upload

---
+++ String_Manipulation1.java
@@ -0,0 +1,19 @@
+package Task3;
+
+public class String_Manipulation1 {
+
+	public static void main(String[] args) {
+		String str1="Python";
+		String str2="python";
+		/*if(str1==str2)
+			System.out.println("str1 and str2 are same");
+		else
+            System.out.println("str1 and str2 are different");*/
+		if(str1.equals(str2))
+			System.out.println("str1 and str2 are same");
+		else
+            System.out.println("str1 and str2 are different");
+		
+	}
+
+}
